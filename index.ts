0aededb9e71d6786f4c4b8fd857686efd011cf9b
Mykhailo Kobernyk <m.kobernyk@gmail.com>
2025-02-16 14:20:54 +0200

    Initial commit: SSR Rendering, SocketIO connected

---
+++ index.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from 'express';
+import * as http from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get('/', (req: Request, res: Response) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('користувач підключився');
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
